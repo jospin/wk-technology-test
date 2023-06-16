e4652f5c88e27dc1f73192bb84951678aad47ce8
Lucien Carbonare Jospin <lucien.carbonare@gmail.com>
2023-06-16 18:03:31 -0300

    structure projet create

---
+++ src/test/java/br/com/wktechnology/knowledge/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wktechnology.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
